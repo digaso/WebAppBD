d89187bdcc2e49cbbc9313440a2865d6b67c5481
digaso <diogomoura2014@gmail.com>
2021-12-28 11:24:03 +0000

    feat: add routes and fix database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from flask.wrappers import Request
 import db
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, url_for
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -7,7 +8,40 @@ logging.basicConfig(level=logging.DEBUG,
 app = Flask(__name__)
 
 
-def mapList(values):
+def getPrimaryKeys(table):
+    keys = []
+    values = getFields(table)
+    for x in range(0, len(values)):
+        if values[x]['Key'] == 'PRI':
+            keys.append(values[x]['Field'])
+    return keys
+
+
+def getFields(table):
+    values = {}
+    res = db.execute('describe ' + table)
+    values = res.fetchall()
+    return values
+
+
+def getData(table):
+    res = db.execute('select * from ' + table)
+    return res.fetchall()
+
+
+def getDatabyId(table, fields, id):
+    keys = getPrimaryKeys(table)
+    res = db.execute('select * from ' + table +
+                     ' where ' + keys[0] + ' = ' + id)
+    return res.fetchall()
+
+
+def getTable(table, id):
+    res = db.execute('select * from ' + table)
+    return res.fetchall()
+
+
+def getTables(values):
     from dotenv import dotenv_values
     db_name = dotenv_values(".env")['DB']
     tables = []
@@ -17,20 +51,41 @@ def mapList(values):
 
 
 @ app.route('/')
-def hello():
-    return render_template('index.html', message='Hello There!')
-
-
-@ app.route('/user')
-def userpage():
-    return render_template('index.html', message='Hello User!')
-
-
 @ app.route('/tables')
 def tablespage():
     values = {}
     res = db.execute('show Tables')
     values = res.fetchall()
-    tables = mapList(values)
+    tables = getTables(values)
 
     return render_template('tables.html', title='Tables', tables=tables)
+
+
+@ app.route('/tables/<table>')
+def tableInfo(table):
+    fields = {}
+    fields = getFields(table)
+    obj = fields[0]
+    keys = list(obj.keys())
+
+    return render_template('tableInfo.html', table=table, fields=fields, keys=keys)
+
+
+@app.route('/tables/<table>/data')
+def tableData(table):
+    data = {}
+    data = getData(table)
+    obj = data[0]
+    fields = list(obj.keys())
+    keys = getPrimaryKeys(table)
+    return render_template('table.html', table=table, data=data, fields=fields, keys=keys)
+
+
+@app.route('/tables/<table>/data/<id>')
+def tableDataId(table, id):
+    fields = {}
+    fields = getFields(table)
+    data = {}
+    data = getDatabyId(table, fields, id)
+
+    return render_template('item.html', table=table, data=data, fields=fields)
